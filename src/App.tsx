5e645e2005ca36fd76c6440f91d30c508a193391
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-07-08 17:15:13 -0300

    Adicionado a logica da IA com n8n

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,79 @@
 import "./styles/main.scss";
-import { Section } from "./components/Section";
+import { CardIA } from "./components/CardIA";
+import { useState } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [codeResult, setCodeResult] = useState("Mostrar codigo gerado...");
+  const [animResult, setAnimResult] = useState("Mostrar Animação");
+  const [isLoading, setIsLoading] = useState(false);
+  const webhook = "https://igorbern.app.n8n.cloud/webhook/animacao-css";
+
+  const handleClick = async () => {
+    setIsLoading(true); // começa o loading
+
+    try {
+      const res = await fetch(webhook, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ pergunta: inputValue }),
+      });
+
+      const data = await res.json();
+      const info = JSON.parse(data.resposta);
+
+      setCodeResult(info.code);
+      setAnimResult(info.preview);
+
+      const styleTag = document.createElement("style");
+      styleTag.innerHTML = info.style;
+      document.head.appendChild(styleTag);
+    } catch (error) {
+      setCodeResult("Erro ao gerar código");
+      setAnimResult("Erro ao gerar animação");
+      console.error("Erro ao buscar animação:", error);
+    } finally {
+      setIsLoading(false); // termina o loading
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleClick();
+    }
+  };
+
   return (
     <>
       <header className="header">
         <h1 className="title">Inteligência Artificial para gerar CSS</h1>
       </header>
-      <div className="sections">
-        <Section text="Mostrar codigo gerado" />
-        <Section text="Mostrar animação" />
-      </div>
+      <main className="main">
+        <section className="section_join">
+          <input
+            type="text"
+            className="input"
+            placeholder="Digite a animação que você deseja..."
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            disabled={isLoading}
+            onKeyDown={handleKeyDown}
+          />
+          <button className="botao" onClick={handleClick}>
+            {isLoading ? "Gerando..." : "Gerar animação"}
+          </button>
+        </section>
+        <section className="section_result">
+          <CardIA text={codeResult} isDark showCopyButton />
+          <CardIA>
+            {isLoading ? (
+              <p>Carregando animação...</p>
+            ) : (
+              <p dangerouslySetInnerHTML={{ __html: animResult }} />
+            )}
+          </CardIA>
+        </section>
+      </main>
     </>
   );
 }
---
+++ src/components/CardIA.tsx
@@ -0,0 +1,40 @@
+import "../styles/components/_card-ia.scss";
+import { useState, type ReactNode } from "react";
+import { FaClipboard } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
+
+type CardIAProps = {
+  text?: string;
+  children?: ReactNode;
+  isDark?: boolean;
+  showCopyButton?: boolean;
+};
+
+export const CardIA = ({
+  text,
+  children,
+  isDark,
+  showCopyButton,
+}: CardIAProps) => {
+  const [isCheck, setIsCheck] = useState(false);
+
+  const handleCopy = () => {
+    if (text) {
+      setIsCheck(true);
+      navigator.clipboard.writeText(text);
+      setTimeout(() => setIsCheck(false), 2500);
+    }
+  };
+
+  return (
+    <section className={`card-ia ${isDark ? "card-ia--dark" : ""}`}>
+      {showCopyButton && (
+        <button onClick={handleCopy} className="copy-button">
+          {isCheck ? <FaCheck /> : <FaClipboard />}
+        </button>
+      )}
+      {text && <div>{text}</div>}
+      {children && <div>{children}</div>}
+    </section>
+  );
+};
