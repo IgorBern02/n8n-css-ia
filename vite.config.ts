263a0c3d42f35caff724eba39f1893b53814dbaa
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-07-08 22:09:43 -0300

    Adicionado base em vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/n8n-css-ia/", // ✅ Adicione essa linha
   plugins: [react()],
-})
+});
